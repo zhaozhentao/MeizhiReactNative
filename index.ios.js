bc7a8ae47a7d71300b9d6b0f26d24435a4f29669
zhaozhentao <514656282@qq.com>
2015-09-29 17:35:58 +0800

    modify index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
+var Home = require('./js/home');
 var {
   AppRegistry,
   StyleSheet,
@@ -15,18 +12,8 @@ var {
 var MeizhiReactNative = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Home>
+      </Home>
     );
   }
 });
